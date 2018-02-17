4081149edc1ec4fcd246aa07a3d3059b89c8a697
Mai Alzamel <k1505471@kcl.ac.uk>
2018-02-17 18:23:15 +0000

    Update mapdefs.h

--- mapdefs.h
+++ mapdefs.h
@@ -15,10 +15,27 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
+
+/**
+ K-MAP: In the k-mappability problem, we are given a string x of length n and integers m and k, and we are asked to count, for each length-m factor y of x, the number of other factors of length m of x that are at Hamming distance at most k from y.Copyright (C) 2017 Mai Alzamel.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 #define ALLOC_SIZE              1048576
 #define DEL                     '$'
 #define DEL_STR                 "$"
-
 #define DNA                     "ACGTN"                         //DNA alphabet
 #define PROT                    "ARNDCQEGHILKMFPSTWYVOUBZJX*"          //Proteins alphabet
 #define IUPAC                   "ACGTUWSMKRYBDHVN"          	//IUPAC alphabet
@@ -38,7 +55,7 @@ struct TSwitch
    char               * alphabet;
    char               * input_filename;
    char               * output_filename;
-   unsigned int         k,m;
+   unsigned int         k, m, T;
    unsigned int         total_length;
  };
 struct Range {
@@ -76,6 +93,13 @@ unsigned int compute_naive_at_most_k_map ( unsigned char * seq, unsigned char * 
 
 unsigned int Counting_Blocks ( INT p, INT L,INT l_1, INT m);
 unsigned int Counting_Blocks_for_Two_Mismatch ( INT p, INT L,INT mismatch,INT mismatch_1, INT m);
+unsigned int compute_At_Most_K_map_simple ( unsigned char * seq,unsigned char * seq_id, struct TSwitch sw, INT m ,   double  * C ,int k);
+unsigned int Counting_Blocks_for_K_Mismatch ( INT p, INT L,INT * mismatches, INT m, INT k) ;
+
+
+unsigned int compute_At_Most_K_map_simple_mutlthreads ( unsigned char * seq,unsigned char * seq_id, struct TSwitch sw, INT m ,   double  * C ,int k);
+
+
 
 
 
